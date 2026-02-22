390c25e661b5ab19c5bd859db00a86b23be3ba4a
Brian Scott <191290+bscott@users.noreply.github.com>
2026-02-22 08:59:54 -0800

    Bump version to 0.2.0

--- internal/cli/cli.go
+++ internal/cli/cli.go
@@ -5,7 +5,7 @@ import (
 	"github.com/bscott/pm-cli/internal/output"
 )
 
-var Version = "0.1.0"
+var Version = "0.2.0"
 
 type Globals struct {
 	JSON     bool   `help:"Output as JSON" name:"json"`
